156eb69b0ca73d94c4089f66b45c8f2474a3d3c5
Dmitry Bukavin <djaconda@bitfocus.com>
2017-03-12 22:22:58 +0200

    final application

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,33 +1,55 @@
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {DebugElement, Component, NgModule} from '@angular/core';
+import {RouterTestingModule} from '@angular/router/testing';
 
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By }           from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+@Component({
+    template: ''
+})
+class MockLoginComponent {
+}
+
+@NgModule({
+    declarations: [MockLoginComponent],
+    exports: [MockLoginComponent]
+})
+class MockModule {
+}
 
 describe('AppComponent', function () {
-  let de: DebugElement;
-  let comp: AppComponent;
-  let fixture: ComponentFixture<AppComponent>;
+    let de: DebugElement;
+    let comp: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AppComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [AppComponent],
+            imports: [
+                MockModule,
+                RouterTestingModule.withRoutes([
+                    {
+                        path: 'login',
+                        component: MockLoginComponent
+                    }
+                ]),
+            ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h1'));
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AppComponent);
+        comp = fixture.componentInstance;
+        de = fixture.debugElement.query(By.css('h1'));
+    });
 
-  it('should create component', () => expect(comp).toBeDefined() );
+    it('should create component', () => expect(comp).toBeDefined());
 
-  it('should have expected <h1> text', () => {
-    fixture.detectChanges();
-    const h1 = de.nativeElement;
-    expect(h1.innerText).toMatch(/angular/i,
-      '<h1> should say something about "Angular"');
-  });
+    it('should have expected <h1> text', () => {
+        fixture.detectChanges();
+        const h1 = de.nativeElement;
+        expect(h1.innerText).toMatch(/Heroes/i,
+            '<h1> should say something about "Heroes"');
+    });
 });
